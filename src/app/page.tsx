6fa17506d64da2a9614a5ca4e6638db8158e6107
sahilmachhi29 <sahilmachhi29@gmail.com>
2025-01-14 16:27:39 +0000

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import { Spotlight } from "@/components/ui/Spotlight";
+import SparklesText from "@/components/ui/sparkles-text";
 
 export default function Home() {
   return (
     <>
-      <Spotlight />
+    <section className="relative">
+    <Spotlight className="-top-[200px] left-[580px]"/>
       <div className="items-center flex justify-center w-full mt-[200px]">
         <h1 className="text-7xl text-white/70 font-bold text-center max-w-3xl">
-          Celebrate fitness goal with your mates with same{" "}
+          Celebrate fitness goal with your mates with same {" "}
           <span className="text-black bg-white/70 rounded-full px-6">
-            Mindset
+          <SparklesText text="Mindset" className="text-7xl"/>
           </span>
         </h1>
       </div>
+    </section>
     </>
   );
 }
